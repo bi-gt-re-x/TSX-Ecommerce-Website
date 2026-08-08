edf97dba6ffd05b5a31306b1a5fca43798d56f5a
Myles Zhang <hanwenks@gmail.com>
2026-08-07 21:24:29 -0500

    Install more packages

---
+++ Chatbot Project/src/App.jsx
@@ -0,0 +1,121 @@
+import { useState } from 'react';
+import {Chatbot} from 'supersimpledev';
+import './App.css'
+
+function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState('');
+
+  function saveInputText(event) {
+    setInputText(event.target.value);
+  }
+
+  async function sendMessage(event) {
+    if (event.key === 'Enter' || event.type === 'click') {
+        const newChatMessages = ([
+        ...chatMessages,
+        {
+            message: inputText,
+            sender: 'user',
+            id: crypto.randomUUID(),
+        }
+        ]);
+
+        if (!inputText.trim()) return; 
+
+        const response = await Chatbot.getResponseAsync(inputText);
+
+        setChatMessages([
+        ...newChatMessages,
+        {
+            message: response,
+            sender: 'robot',
+            id: crypto.randomUUID(),
+        }
+        ]);
+
+        setInputText(''); 
+    }
+
+    else if (event.key === 'Escape') {
+        setInputText('');
+    }
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input 
+        placeholder="Send a Message to Chatbot" 
+        size="30" 
+        value={inputText} 
+        onChange={saveInputText}
+        onKeyDown={sendMessage}
+        className="chat-input"
+      />
+      <button onClick={sendMessage} className="send-button">Send</button>
+    </div>
+  );
+}
+
+function ChatMessage(props) {
+  const message = props.message;
+  const sender = props.sender;
+  if (sender === 'robot') {
+    return (
+      <div className={sender === 'user' ? 'chat-message-user' : 'chat-message-robot'}>
+        <img className="chat-message-profile" src="robot_image.png" />
+        <div className="chat-message-text">
+          {message}
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={sender === 'user' ? 'chat-message-user' : 'chat-message-robot'}>
+        <div className="chat-message-text">
+          {message}
+        </div>
+        <img className="chat-message-profile" src="person_image.png"/>
+      </div>
+    );
+  }
+}
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const containerElem = chatMessagesRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+  }, [chatMessages]);
+
+  return (
+    <div className="chat-messages-container"
+    ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage message={chatMessage.message} sender={chatMessage.sender} key={chatMessage.id} />
+        )
+      })}
+    </div>
+  );
+}
+
+function App() {
+  const [chatMessages, setChatMessages] = useState([
+    { message: 'Hello Chatbot', sender: 'user', id: 'id1' },
+    { message: 'Hello! How can I help you?', sender: 'robot', id: 'id2' },
+    { message: "Can you give me Today's date?", sender: 'user', id: 'id3' },
+    { message: "Today is August 2nd", sender: 'robot', id: 'id4' }
+  ]);
+
+  return (
+    <div className="app-container">
+      <ChatMessages chatMessages={chatMessages} />
+      <ChatInput chatMessages={chatMessages} setChatMessages={setChatMessages} />
+    </div>
+  );
+}
+
+export default App
