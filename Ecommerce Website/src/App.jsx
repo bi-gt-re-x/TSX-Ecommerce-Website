b0f04e69c8264bf0bab5686344f29929b338e334
Myles Zhang <hanwenks@gmail.com>
2026-08-10 13:13:02 -0500

    Fix Routing

--- Ecommerce Website/src/App.jsx
+++ Ecommerce Website/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './Pages/HomePage';
 import { CheckoutPage } from './Pages/CheckoutPage';
+import { OrdersPage } from './Pages/OrdersPage';
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="orders" element={<OrdersPage />}/>
     </Routes>
   )
 }
