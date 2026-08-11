18dc349cadc84f3d5fb70677abbdf738a3d63098
Myles Zhang <hanwenks@gmail.com>
2026-08-10 20:40:25 -0500

    Fix ESLint Errors

--- Ecommerce Website/src/App.jsx
+++ Ecommerce Website/src/App.jsx
@@ -1,19 +1,19 @@
 import { Routes, Route } from 'react-router';
-import { HomePage } from './Pages/HomePage';
-import { CheckoutPage } from './Pages/CheckoutPage';
-import { OrdersPage } from './Pages/OrdersPage';
-import { TrackingPage } from './Pages/TrackingPage';
+import { HomePage } from './Pages/HomePage.jsx';
+import { CheckoutPage } from './Pages/CheckoutPage.jsx';
+import { OrdersPage } from './Pages/OrdersPage.jsx';
+import { TrackingPage } from './Pages/TrackingPage.jsx';
 import './App.css'
 
 function App() {
-  return (
-    <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
-      <Route path="orders" element={<OrdersPage />}/>
-      <Route path="tracking" element={<TrackingPage />} />
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route index element={<HomePage />} />
+            <Route path="checkout" element={<CheckoutPage />} />
+            <Route path="orders" element={<OrdersPage />}/>
+            <Route path="tracking" element={<TrackingPage />} />
+        </Routes>
+    )
 }
 
 export default App
--- Ecommerce Website/src/components/Header.jsx
+++ Ecommerce Website/src/components/Header.jsx
@@ -7,10 +7,10 @@ export function Header() {
             <div className="header">
                 <div className="left-section">
                     <Link to="/" className="header-link">
-                    <img className="logo"
-                        src="images/logo-white.png" />
-                    <img className="mobile-logo"
-                        src="images/mobile-logo-white.png" />
+                        <img className="logo"
+                            src="images/logo-white.png" />
+                        <img className="mobile-logo"
+                            src="images/mobile-logo-white.png" />
                     </Link>
                 </div>
 
@@ -18,20 +18,19 @@ export function Header() {
                     <input className="search-bar" type="text" placeholder="Search" />
 
                     <button className="search-button">
-                    <img className="search-icon" src="images/icons/search-icon.png" />
+                        <img className="search-icon" src="images/icons/search-icon.png" />
                     </button>
                 </div>
 
                 <div className="right-section">
                     <Link className="orders-link header-link" to="/orders">
-
-                    <span className="orders-text">Orders</span>
+                        <span className="orders-text">Orders</span>
                     </Link>
 
                     <Link className="cart-link header-link" to="/checkout">
-                    <img className="cart-icon" src="images/icons/cart-icon.png" />
-                    <div className="cart-quantity">3</div>
-                    <div className="cart-text">Cart</div>
+                        <img className="cart-icon" src="images/icons/cart-icon.png" />
+                        <div className="cart-quantity">3</div>
+                        <div className="cart-text">Cart</div>
                     </Link>
                 </div>
             </div>
