fe763d04717577da1ef48c41126a545e9d55fde7
Myles Zhang <hanwenks@gmail.com>
2026-08-10 20:51:52 -0500

    Install Axios

--- Ecommerce Website/src/Pages/HomePage.jsx
+++ Ecommerce Website/src/Pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import axios from 'axios';
 import { Header } from '../components/Header';
 import { products } from '../starting code/data/products';
 import './HomePage.css';
 
 export function HomePage() {
+    axios.get('http://localhost:3000/api/products').then((response) => {
+        return response.json();
+    });
+
     return (
         <>
             <title>Ecommerce Project</title>
